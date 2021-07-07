a3bd8d0fc08ec8b5ed70040a2d8962196ce5fee5
Nikhil Ramesh <32214184+Iamnikhilramesh@users.noreply.github.com>
2021-07-07 19:11:12 +0200

    Update me.py

--- me.py
+++ me.py
@@ -20,14 +20,13 @@ def about_me():
     #About me
     my_expander = st.beta_expander("A few words about me", expanded=True)
     with my_expander:
-        st.header("I’m Nikhil Ramesh, a multidisciplinary thinker who focuses on telling my clients stories with statistics, through enjoyable and meaningful experiences.      I specialize in Machine learning algorithms, Data Pipelines and Data visualization tools.")
+        st.subheader("I’m Nikhil Ramesh, a multidisciplinary thinker who focuses on telling my clients stories with statistics, through enjoyable and meaningful experiences.      I specialize in Machine learning algorithms, Data Pipelines and Data visualization tools.")
         
     my_expander3 = st.beta_expander("Some of the Clients I have worked with", expanded=True)
     
     
     with my_expander3:
         st.subheader("Over the past 4 years I have been working with companies and startups around the world as a developer and Data Scientist, working solo and leading small smart application.")
-        st.markdown("</br></br>")
         d,e = st.beta_columns(2)
         d.image("isha.jpeg",caption="Isha Foundations")
         e.image("nccab.png",caption="NCC AB")
